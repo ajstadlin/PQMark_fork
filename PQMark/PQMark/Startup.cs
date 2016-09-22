562d7abe618e63f823cdea735211637bdfbf8dc3
Billy Ernest <bernest@gridprotectionalliance.org>
2016-09-22 11:20:58 -0400

    Initial Commit

---
+++ PQMark/PQMark/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PQMark.Startup))]
+namespace PQMark
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
