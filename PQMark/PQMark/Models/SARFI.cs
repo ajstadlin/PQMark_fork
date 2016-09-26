30573d0cec3fbf16c744ae15a27a01832e33071a
Billy Ernest <bernest@gridprotectionalliance.org>
2016-09-26 14:26:49 -0400

    Fixed yaxis

---
+++ PQMark/PQMark/Models/SARFI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GSF.Data.Model;
+
+namespace PQMark.Models
+{
+    public class SARFI
+    {
+        [PrimaryKey(true)]
+        public int ID { get; set; }
+        public int IndexID { get; set; }
+        public int SiteID { get; set; }
+        public float Count { get; set; } 
+
+    }
+}
