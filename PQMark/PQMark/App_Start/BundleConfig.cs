cc7387de631a1637c44e2cdb584075d446f6516d
Billy Ernest <bernest@gridprotectionalliance.org>
2016-10-31 13:14:14 -0400

    added epri styling to the layout

--- PQMark/PQMark/App_Start/BundleConfig.cs
+++ PQMark/PQMark/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace PQMark
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/default.css"));
         }
     }
 }
